782d1e1cfdd81d71a7fd0358f89863a12a18b277
Emmanuel Ikpe <97067863+MUFFIUS@users.noreply.github.com>
2022-03-14 02:28:00 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "my-def-tf-state-bucket"
     key = "main"
-    region = "us-east-2"
+    region = "us-east-1"
     dynamodb_table = "my-Dynomodb-table1"
   }
 }
